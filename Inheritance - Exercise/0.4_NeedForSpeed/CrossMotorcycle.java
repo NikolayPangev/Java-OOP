629f3e473657a0acd95b7d53597af620f53dfd29
Nikolay Pangev <nikolai.pangev3717@gmail.com>
2023-08-03 18:44:25 +0300

    Inheritance - Exercise

---
+++ Inheritance - Exercise/0.4_NeedForSpeed/CrossMotorcycle.java
@@ -0,0 +1,8 @@
+package Inheritance_Exercise.NeedForSpeed;
+
+public class CrossMotorcycle extends Motorcycle{
+
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
